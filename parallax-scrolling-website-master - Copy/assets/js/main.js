fec992679d339699b4d02efa92f84d93dfb4cb7f
vaggelisdrak <71725114+vaggelisdrak@users.noreply.github.com>
2023-05-14 17:31:35 +0300

    Add files via upload

---
+++ parallax-scrolling-website-master - Copy/assets/js/main.js
@@ -0,0 +1,103 @@
+
+
+// HEADER
+
+const header = document.getElementById('head');
+
+const activeElementOnScroll = function () {
+    if (window.scrollY > 70) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
+
+/*====== MENU =====*/
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+    for (let i = 0; i < revealElements.length; i++) {
+        const elementIsInScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15;
+
+        if (elementIsInScreen) {
+            revealElements[i].classList.add("revealed");
+        } else {
+            revealElements[i].classList.remove("revealed");
+        }
+    }
+}
+
+window.addEventListener("scroll", scrollReveal);
+
+scrollReveal();
+
+/*====== RELLAX =====*/
+var rellax = new Rellax('.parallax');
+
+
+/*====== ANIMATE GSAP ======*/
+/*Navbar*/
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: .5, y: 30, ease:'expo.out'});
+gsap.from('.nav__toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
+
+/*Text*/
+gsap.from('.home__title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
+gsap.from('.home__subtitle', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
+
+/*Scroll*/
+gsap.from('.home__scroll', {opacity:0, duration: 3, delay: 1.5, y: 25, ease:'expo.out'});
+
+
+/*====== SCROLL REVEAL SECTION ======*/
+const sr = ScrollReveal({
+    duration: 2500,
+    reset: true
+});
+
+/*Data*/
+sr.reveal('.section__data',{origin: 'left',distance: '70px'}); 
+
+/*Imgs*/
+sr.reveal('.section__img',{origin: 'left',distance: '90px',delay: 200}); 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+console.log(sections)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 158,
+            sectionId = current.getAttribute('id')
+            console.log(sectionId)
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
